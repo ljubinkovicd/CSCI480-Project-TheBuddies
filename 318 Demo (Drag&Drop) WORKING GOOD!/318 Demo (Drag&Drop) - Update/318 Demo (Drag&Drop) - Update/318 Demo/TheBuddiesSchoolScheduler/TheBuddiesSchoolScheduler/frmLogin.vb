9dbc64a3e0b7496567ff2f14dd59e9156efe4787
Djordje Ljubinkovic <ljubinkovicd@findlay.edu>
2015-03-19 17:27:58 -0400

    Drag and Drop committed
    Finally figured out how to make drag and drop accurate. No unexpected
    behavior for now.

---
+++ 318 Demo (Drag&Drop) WORKING GOOD!/318 Demo (Drag&Drop) - Update/318 Demo (Drag&Drop) - Update/318 Demo/TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/frmLogin.vb
@@ -0,0 +1,28 @@
+﻿Public Class frmLogin
+
+    ' TODO: Insert code to perform custom authentication using the provided username and password 
+    ' (See http://go.microsoft.com/fwlink/?LinkId=35339).  
+    ' The custom principal can then be attached to the current thread's principal as follows: 
+    '     My.User.CurrentPrincipal = CustomPrincipal
+    ' where CustomPrincipal is the IPrincipal implementation used to perform authentication. 
+    ' Subsequently, My.User will return identity information encapsulated in the CustomPrincipal object
+    ' such as the username, display name, etc.
+
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        Dim SQL As New SQLConnect
+        SQL.Connect(txtServer.Text, txtUser.Text, txtPassword.Text)
+
+        If SQL.HasConnection Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        Me.Close()
+        'frmWelcome.Close()
+    End Sub
+
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ 318 Demo (Drag&Drop) WORKING GOOD!/318 Demo (Drag&Drop) - Update/318 Demo (Drag&Drop) - Update/318 Demo/TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/frmTeacherSchedule.vb
@@ -0,0 +1,16 @@
+﻿Public Class frmTeacherSchedule
+
+   
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Me.Show()
+        frmTeacherSpecs.ShowDialog()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
+End Class
