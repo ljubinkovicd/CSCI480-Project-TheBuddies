712b02b406cf8efb93506aff89b6680c1152cb45
Chris Reaper <reaperc@findlay.edu>
2015-04-11 21:50:28 -0400

    Added save user name, database, and server
    Added save user name, database, and server functionality to login
    screen. Saves off to a public text file.

--- TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/frmLogin.Designer.vb
+++ TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/frmLogin.Designer.vb
@@ -42,6 +42,7 @@ Partial Class frmLogin
         Me.txtServer = New System.Windows.Forms.TextBox()
         Me.txtDatabase = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.chkSaveInfo = New System.Windows.Forms.CheckBox()
         CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -49,100 +50,122 @@ Partial Class frmLogin
         '
         Me.LogoPictureBox.Image = CType(resources.GetObject("LogoPictureBox.Image"), System.Drawing.Image)
         Me.LogoPictureBox.Location = New System.Drawing.Point(0, 0)
+        Me.LogoPictureBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.LogoPictureBox.Name = "LogoPictureBox"
-        Me.LogoPictureBox.Size = New System.Drawing.Size(165, 193)
+        Me.LogoPictureBox.Size = New System.Drawing.Size(248, 297)
         Me.LogoPictureBox.TabIndex = 0
         Me.LogoPictureBox.TabStop = False
         '
         'UsernameLabel
         '
-        Me.UsernameLabel.Location = New System.Drawing.Point(171, 104)
+        Me.UsernameLabel.Location = New System.Drawing.Point(256, 160)
+        Me.UsernameLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.UsernameLabel.Name = "UsernameLabel"
-        Me.UsernameLabel.Size = New System.Drawing.Size(220, 23)
+        Me.UsernameLabel.Size = New System.Drawing.Size(330, 35)
         Me.UsernameLabel.TabIndex = 0
         Me.UsernameLabel.Text = "&User name"
         Me.UsernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'PasswordLabel
         '
-        Me.PasswordLabel.Location = New System.Drawing.Point(171, 151)
+        Me.PasswordLabel.Location = New System.Drawing.Point(256, 232)
+        Me.PasswordLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.PasswordLabel.Name = "PasswordLabel"
-        Me.PasswordLabel.Size = New System.Drawing.Size(220, 23)
+        Me.PasswordLabel.Size = New System.Drawing.Size(330, 35)
         Me.PasswordLabel.TabIndex = 2
         Me.PasswordLabel.Text = "&Password"
         Me.PasswordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'txtUser
         '
-        Me.txtUser.Location = New System.Drawing.Point(171, 129)
+        Me.txtUser.Location = New System.Drawing.Point(256, 198)
+        Me.txtUser.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txtUser.Name = "txtUser"
-        Me.txtUser.Size = New System.Drawing.Size(220, 20)
+        Me.txtUser.Size = New System.Drawing.Size(328, 26)
         Me.txtUser.TabIndex = 2
         '
         'txtPassword
         '
-        Me.txtPassword.Location = New System.Drawing.Point(171, 176)
+        Me.txtPassword.Location = New System.Drawing.Point(256, 271)
+        Me.txtPassword.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txtPassword.Name = "txtPassword"
         Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.txtPassword.Size = New System.Drawing.Size(220, 20)
+        Me.txtPassword.Size = New System.Drawing.Size(328, 26)
         Me.txtPassword.TabIndex = 3
         '
         'OK
         '
-        Me.OK.Location = New System.Drawing.Point(197, 209)
+        Me.OK.Location = New System.Drawing.Point(296, 367)
+        Me.OK.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.OK.Name = "OK"
-        Me.OK.Size = New System.Drawing.Size(94, 23)
+        Me.OK.Size = New System.Drawing.Size(141, 35)
         Me.OK.TabIndex = 4
         Me.OK.Text = "&OK"
         '
         'Cancel
         '
         Me.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.Cancel.Location = New System.Drawing.Point(300, 209)
+        Me.Cancel.Location = New System.Drawing.Point(450, 367)
+        Me.Cancel.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Cancel.Name = "Cancel"
-        Me.Cancel.Size = New System.Drawing.Size(94, 23)
+        Me.Cancel.Size = New System.Drawing.Size(141, 35)
         Me.Cancel.TabIndex = 5
         Me.Cancel.Text = "&Cancel"
         '
         'Label1
         '
-        Me.Label1.Location = New System.Drawing.Point(171, 9)
+        Me.Label1.Location = New System.Drawing.Point(256, 14)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(220, 23)
+        Me.Label1.Size = New System.Drawing.Size(330, 35)
         Me.Label1.TabIndex = 6
         Me.Label1.Text = "&Server"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'txtServer
         '
-        Me.txtServer.Location = New System.Drawing.Point(171, 33)
+        Me.txtServer.Location = New System.Drawing.Point(256, 51)
+        Me.txtServer.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txtServer.Name = "txtServer"
-        Me.txtServer.Size = New System.Drawing.Size(220, 20)
+        Me.txtServer.Size = New System.Drawing.Size(328, 26)
         Me.txtServer.TabIndex = 0
         '
         'txtDatabase
         '
-        Me.txtDatabase.Location = New System.Drawing.Point(171, 80)
+        Me.txtDatabase.Location = New System.Drawing.Point(256, 123)
+        Me.txtDatabase.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txtDatabase.Name = "txtDatabase"
-        Me.txtDatabase.Size = New System.Drawing.Size(220, 20)
+        Me.txtDatabase.Size = New System.Drawing.Size(328, 26)
         Me.txtDatabase.TabIndex = 1
         '
         'Label2
         '
-        Me.Label2.Location = New System.Drawing.Point(171, 56)
+        Me.Label2.Location = New System.Drawing.Point(256, 86)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(220, 23)
+        Me.Label2.Size = New System.Drawing.Size(330, 35)
         Me.Label2.TabIndex = 8
         Me.Label2.Text = "&Database"
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
+        'chkSaveInfo
+        '
+        Me.chkSaveInfo.AutoSize = True
+        Me.chkSaveInfo.Location = New System.Drawing.Point(256, 324)
+        Me.chkSaveInfo.Name = "chkSaveInfo"
+        Me.chkSaveInfo.Size = New System.Drawing.Size(318, 24)
+        Me.chkSaveInfo.TabIndex = 9
+        Me.chkSaveInfo.Text = "Save Server, Database, and User Name"
+        Me.chkSaveInfo.UseVisualStyleBackColor = True
+        '
         'frmLogin
         '
         Me.AcceptButton = Me.OK
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.Cancel
-        Me.ClientSize = New System.Drawing.Size(402, 246)
+        Me.ClientSize = New System.Drawing.Size(615, 420)
+        Me.Controls.Add(Me.chkSaveInfo)
         Me.Controls.Add(Me.txtDatabase)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.txtServer)
@@ -155,6 +178,7 @@ Partial Class frmLogin
         Me.Controls.Add(Me.UsernameLabel)
         Me.Controls.Add(Me.LogoPictureBox)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "frmLogin"
@@ -170,5 +194,6 @@ Partial Class frmLogin
     Friend WithEvents txtServer As System.Windows.Forms.TextBox
     Friend WithEvents txtDatabase As System.Windows.Forms.TextBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents chkSaveInfo As System.Windows.Forms.CheckBox
 
 End Class
--- TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/frmLogin.vb
+++ TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/frmLogin.vb
@@ -1,4 +1,6 @@
-﻿Public Class frmLogin
+﻿Imports System.IO
+
+Public Class frmLogin
 
     ' TODO: Insert code to perform custom authentication using the provided username and password 
     ' (See http://go.microsoft.com/fwlink/?LinkId=35339).  
@@ -8,12 +10,33 @@
     ' Subsequently, My.User will return identity information encapsulated in the CustomPrincipal object
     ' such as the username, display name, etc.
 
+    ' Probably should find another path, but couldn't access program files.
+    Public filepath As String = "C:\Users\Public\Documents\BuddiesScheduler\saveinfo.txt"
+    Public folderpath As String = "C:\Users\Public\Documents\BuddiesScheduler"
+
     Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
         Dim SQL As New SQLConnect
+
         SQL.Connect(txtServer.Text, txtDatabase.Text, txtUser.Text, txtPassword.Text)
 
         If SQL.HasConnection Then
             Me.Close()
+        End If
+
+        If chkSaveInfo.Checked Then
+            If (Not System.IO.Directory.Exists(folderpath)) Then
+                System.IO.Directory.CreateDirectory(folderpath)
+            End If
+
+            If File.Exists(filepath) Then
+                File.Delete(filepath)
+            End If
+
+            Dim objWriter As New System.IO.StreamWriter(filepath, True)
+            objWriter.WriteLine(txtServer.Text)
+            objWriter.WriteLine(txtDatabase.Text)
+            objWriter.WriteLine(txtUser.Text)
+            objWriter.Close()
         End If
     End Sub
 
@@ -23,6 +46,16 @@
     End Sub
 
     Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim lines As String = ""
+
+
+        If File.Exists(filepath) Then
+            Using tr As TextReader = New StreamReader(filepath)
+                txtServer.Text = Trim(tr.ReadLine())
+                txtDatabase.Text = Trim(tr.ReadLine())
+                txtUser.Text = Trim(tr.ReadLine())
+            End Using
+        End If
 
     End Sub
 End Class
