b42453aad51d8920318864648c2807c8463966e7
smithb3 <smithb3@findlay.edu>
2015-04-26 23:22:18 -0400

    Added Comments
    Finishing Changes and Comments Added.

--- TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/frmClassSpecs.vb
+++ TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/TheBuddiesSchoolScheduler/frmClassSpecs.vb
@@ -1,4 +1,16 @@
-﻿Public Class frmClassSpecs
+﻿'Title: The Buddies School Scheduler
+'Authors: Djordje Ljubinkovic, Alex Moser, Chris Reaper, Baylee Smith
+'Release Date: 4/27/2015
+'Description: The Buddies School Scheduler is a VB.net program made for the client Dr.Geise
+'in CSCI 480 under Dr. Ringenberg. This program is meant to assist Dr.Geise in her semester class
+'scheduling endeavors by allowing her to more easily assess where and when certain classes need to be.
+'We have aided her in providing a drag and drop interface, color-coded room visuals, a running total
+'of faculty hours, and a complete database management system where Dr. Geise can completely customize
+'her experience to her needs. The program also prints out to Microsoft Excel through the "Reports"
+'button on the main screen, which prints out several different versions of the report, including 
+'Weekly, by day, and by faculty.
+
+Public Class frmClassSpecs
     Dim changeDT As New DataTable   'datatable for the changes made
 
     Dim times As New List(Of String) 'list of the times as strings for the conversion from military to normal
